bd987bf4984c5f98596ad813f8a7b0c8e8772c96
pmiaowu <3303003493@qq.com>
2020-12-17 17:15:58 +0800

    payload增加

--- src/main/java/burp/BurpExtender.java
+++ src/main/java/burp/BurpExtender.java
@@ -14,7 +14,7 @@ import burp.CustomErrorException.TaskTimeoutException;
 public class BurpExtender implements IBurpExtender, IScannerCheck {
 
     public static String NAME = "FastJsonScan";
-    public static String VERSION = "1.0.0";
+    public static String VERSION = "1.0.1";
 
     private IBurpExtenderCallbacks callbacks;
     private IExtensionHelpers helpers;
